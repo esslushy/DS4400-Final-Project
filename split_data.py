b0042d6d2fccaec1d80ccf1bdb1e443144b523f6
Claire Schlesinger <esslushy@gmail.com>
2023-04-20 12:44:07 -0400

    data split finished

--- split_data.py
+++ split_data.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 raw_df = pd.read_csv("data/World Happiness Report 2005-2021.csv")
 raw_df.dropna(inplace=True)
 
-train_df, test_df = train_test_split(raw_df, test_size=0.2)
+train_df, test_df = train_test_split(raw_df, test_size=0.2, shuffle=True)
 
 train_df.to_csv("data/train.csv", index=False)
 test_df.to_csv("data/test.csv", index=False)
